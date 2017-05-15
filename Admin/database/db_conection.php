e260dbca0ff3981a11a550edd79f1b1e96e3b705
Kathleen Dianne A. Arbolente <2152544@slu.edu.ph>
2017-05-16 00:15:34 +0800

    app folder

--- Admin/database/db_conection.php
+++
@@ -1,4 +0,0 @@
-<?php    
-$dbcon=mysqli_connect("localhost","root","") or die ("could not connect to mysql"); 
-mysqli_select_db($dbcon,"salon") or die ("no database");  
-?>  
--- Costumer Module/sampleLogin/login/README.txt
+++
@@ -1,3 +0,0 @@
-A Pen created at CodePen.io. You can find this one at http://codepen.io/tmrDevelops/pen/wBJLqj.
-
- Animated circular login form w/loader
